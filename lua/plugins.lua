62131b7cb11c640ab3e164b2672f5850c8912266
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2025-06-19 12:52:38 +0200

    markdown preview plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -146,4 +146,13 @@ require("lazy").setup({
         "nvim-lualine/lualine.nvim",
         dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.cmd [[Lazy load markdown-preview.nvim]]
+            vim.fn["mkdp#util#install"]() 
+        end,
+    },
 })
