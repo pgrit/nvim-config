ac640a8d878e04b0c337113e6403924eb7f07eb7
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2025-07-10 18:04:09 +0200

    fix csharpier config for conform

--- lua/conformsetup.lua
+++ lua/conformsetup.lua
@@ -1,9 +1,15 @@
 require("conform").setup({
+    formatters = {
+        csharpier = {
+            command = "csharpier",
+            args = { "format", "$FILENAME", "--write-stdout" },
+        }
+    },
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "isort", "black" },
 		rust = { "rustfmt", lsp_format = "fallback" },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
-		cs = { "csharpier" },
+		cs = { "csharpier", lsp_format = "fallback" },
 	},
 })
