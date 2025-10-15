d4a73fdc2f1988a4c3773ebc1bbebcf7f0b5bb3f
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2025-10-15 11:39:47 +0200

    tweak statusline & winbar

--- lua/lualinesetup.lua
+++ lua/lualinesetup.lua
@@ -1,7 +1,16 @@
+local auto_theme = require("lualine.themes.auto")
+
+-- auto_theme.normal.c.bg = "#80C6DA"
+-- auto_theme.insert.c.bg = "#80C6DA"
+-- auto_theme.visual.c.bg = "#80C6DA"
+-- auto_theme.replace.c.bg = "#80C6DA"
+-- auto_theme.command.c.bg = "#80C6DA"
+-- auto_theme.inactive.c.bg = "#323742"
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
+		theme = auto_theme,
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
@@ -13,12 +22,22 @@ require("lualine").setup({
 				"dap-repl",
 				"dapui_console",
 				"trouble",
+				"snacks_picker_list",
 			},
-			winbar = {},
+			winbar = {
+				"dapui_scopes",
+				"dapui_breakpoints",
+				"dapui_stacks",
+				"dapui_watches",
+				"dap-repl",
+				"dapui_console",
+				"trouble",
+				"snacks_picker_list",
+			},
 		},
 		ignore_focus = {},
 		always_divide_middle = true,
-		always_show_tabline = true,
+		always_show_tabline = false,
 		globalstatus = false,
 		refresh = {
 			statusline = 1000,
@@ -42,21 +61,35 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = {},
 		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
+		lualine_y = {},
 		lualine_z = { "location" },
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
+		lualine_c = {},
+		lualine_x = {},
 		lualine_y = {},
 		lualine_z = {},
 	},
 	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
+	winbar = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+	inactive_winbar = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
 	extensions = {},
 })
